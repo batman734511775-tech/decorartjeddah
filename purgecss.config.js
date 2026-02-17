d3655bc307c95316045c0ed3d0a7495480d75cae
batman734511775-tech <batman734511775@gmail.com>
2026-02-17 10:45:21 +0300

    Create purgecss.config.js

---
+++ purgecss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "index.html",
+    "neighborhoods/*.html"
+  ],
+  css: ["style.css"],
+  output: "dist",
+  safelist: [
+    "active",
+    "show",
+    "open"
+  ]
+}
